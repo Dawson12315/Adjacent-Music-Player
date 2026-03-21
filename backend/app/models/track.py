021194af061f5c721580fefc1c40f4077cbd0af0
Dawson Hudson <206295493+Dawson12315@users.noreply.github.com>
2026-03-21 18:42:36 -0500

    Add Track model

---
+++ backend/app/models/track.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db import Base
+
+
+class Track(Base):
+    __tablename__ = "tracks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    artist = Column(String, nullable=True)
+    album = Column(String, nullable=True)
+    file_path = Column(String, nullable=False, unique=True)
