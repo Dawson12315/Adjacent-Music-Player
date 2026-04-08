2634bf47158659e3cc1859cbbf1caec5df092fb2
Dawson Hudson <dawsonhudson1999@yahoo.com>
2026-04-08 09:32:43 -0500

    Fix Docker

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,4 @@
-window.APP_CONFIG = {
-  API_BASE_URL: "http://127.0.0.1:8000"
-};
+const API_BASE_URL =
+  window.APP_CONFIG?.API_BASE_URL || 'http://localhost:8000';
+
+export default API_BASE_URL;
